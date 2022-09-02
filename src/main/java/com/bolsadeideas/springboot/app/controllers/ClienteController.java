bbcaaa2916a7d1f2c0252a8dd3f58ea26907cd35
Esteban Medina <jmedina@advisoryservicesg.tech>
2022-09-02 16:24:26 -0500

    Funciones del CRUD terminadas

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -1,18 +1,24 @@
 package com.bolsadeideas.springboot.app.controllers;
 
 import java.util.Map;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.bolsadeideas.springboot.app.models.dao.IClienteDao;
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
 
 @Controller
+@SessionAttributes("cliente")
 public class ClienteController {
 	@Autowired
 	@Qualifier("clienteDao")
@@ -35,8 +41,40 @@ public class ClienteController {
 	
 	
 	@RequestMapping(value="/form", method = RequestMethod.POST)
-	public String guardar(Cliente cliente) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
+		
+		if(result.hasErrors()) {
+			model.addAttribute("titulo", "Formulario de cliente");
+			return "form";
+		}
+		
 		clienteDao.save(cliente);
+		status.setComplete();
 		return "redirect:listar";
 	}
+	
+	@RequestMapping(value ="/form/{id}")
+	public String editar(@PathVariable(value="id") Long id, Map<String, Object> model) {
+		Cliente cliente = null;
+		if(id>0) {
+			cliente = clienteDao.findOne(id);
+			
+		}else {
+			return "redirect:/listar";
+		}
+		model.put("cliente", cliente);
+		model.put("titulo", "Editar cliente");
+		return "form";
+	}
+	
+	@RequestMapping(value ="/eliminar/{id}")
+	public String eliminar(@PathVariable(value = "id") Long id){
+		if(id > 0) {
+			clienteDao.delete(id);
+		}
+		return "redirect:/listar";	
+		
+	}	
+	
+	
 }
