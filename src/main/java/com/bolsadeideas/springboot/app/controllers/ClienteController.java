7e03e0e70dc23cc5c0491182d6f7cf7201f39287
Esteban Medina <jmedina@advisoryservicesg.tech>
2022-09-02 17:01:47 -0500

    Aplicacion terminada con CRUD hecho por mi

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -16,17 +16,17 @@ import org.springframework.web.bind.support.SessionStatus;
 
 import com.bolsadeideas.springboot.app.models.dao.IClienteDao;
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
+import com.bolsadeideas.springboot.app.models.service.IClienteService;
 
 @Controller
 @SessionAttributes("cliente")
 public class ClienteController {
 	@Autowired
-	@Qualifier("clienteDao")
-	private IClienteDao clienteDao;
+	private IClienteService clienteService;
 	
 	@RequestMapping(value ="listar", method=RequestMethod.GET )
 	public String listar(Model model) {
-		model.addAttribute("clientes",clienteDao.findAll());
+		model.addAttribute("clientes",clienteService.findAll());
 		model.addAttribute("titulo", "Listado de clientes");
 		return "listar";
 	}
@@ -48,7 +48,7 @@ public class ClienteController {
 			return "form";
 		}
 		
-		clienteDao.save(cliente);
+		clienteService.save(cliente);
 		status.setComplete();
 		return "redirect:listar";
 	}
@@ -57,7 +57,7 @@ public class ClienteController {
 	public String editar(@PathVariable(value="id") Long id, Map<String, Object> model) {
 		Cliente cliente = null;
 		if(id>0) {
-			cliente = clienteDao.findOne(id);
+			cliente = clienteService.findOne(id);
 			
 		}else {
 			return "redirect:/listar";
@@ -70,7 +70,7 @@ public class ClienteController {
 	@RequestMapping(value ="/eliminar/{id}")
 	public String eliminar(@PathVariable(value = "id") Long id){
 		if(id > 0) {
-			clienteDao.delete(id);
+			clienteService.delete(id);
 		}
 		return "redirect:/listar";	
 		
