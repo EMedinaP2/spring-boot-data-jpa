d912ecd52b5e97afa8be651c656c590650c1dcb6
Esteban Medina <jmedina@advisoryservicesg.tech>
2022-09-02 13:01:01 -0500

    Primer  commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+/* Populate tables */
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (1, 'Esteban', 'Medina', 'estmed@gmail.com', '2022-09-02');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (2, 'Javier', 'Martinez', 'xavs233@gmail.com', '2022-10-27');
